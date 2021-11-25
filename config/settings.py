bea107d5147eda57b510fe3dd0c0d013238e263d
ProbablyX <83953506+davdtheemonk@users.noreply.github.com>
2021-11-25 16:45:51 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,7 @@ django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DJANGO_SETTINGS_MODULE=config.settings.production
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
